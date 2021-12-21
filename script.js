5b8e6460a67a5c10c820dc1e9b154c0548b2e430
roke741 <jhordieroque741@gmail.com>
2021-12-21 00:29:57 -0500

    primera version, calculadora en javascript

---
+++ script.js
@@ -0,0 +1,47 @@
+
+function borrar(){
+    var caja = document.getElementById("operaciones");
+    //caja.style.display = "none";
+    caja.innerHTML = "";
+}
+
+//mostrar los numeros clickeados
+function mostrar(numero){  
+    var caja = document.getElementById("operaciones");
+    caja.style.display = "block";
+    caja.innerHTML += numero;
+}
+function decimal(){
+    var caja = document.getElementById("operaciones");
+    caja.style.display = "block";
+    caja.innerHTML += ".";
+}
+
+function sumar(){
+    var caja = document.getElementById("operaciones");
+    caja.style.display = "block";
+    caja.innerHTML += "+";
+}
+function restar(){
+    var caja = document.getElementById("operaciones");
+    caja.style.display = "block";
+    caja.innerHTML += "-";
+}
+function multiplicar(){
+    var caja = document.getElementById("operaciones");
+    caja.style.display = "block";
+    caja.innerHTML += "x";
+}
+function dividir(){
+    var caja = document.getElementById("operaciones");
+    caja.style.display = "block";
+    caja.innerHTML += "/";
+}
+
+function calcular(){
+    var caja = document.getElementById("operaciones");
+    var resultados = eval(caja.innerHTML);
+    //var pequeño = caja.innerHTML;
+    //document.getElementById("resultados").innerHTML = pequeño;
+    caja.innerHTML = "= " + resultados;
+}
